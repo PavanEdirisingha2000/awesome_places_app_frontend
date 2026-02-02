bb581a3c2536d40c6069f1e1b10ae55c7899897b
Pavan Edirisingha <96134390+PavanEdirisingha2000@users.noreply.github.com>
2026-02-02 13:00:53 +0530

    colous update

---
+++ lib/utils/colors.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+final Color MainTextColor = Color(0xFF3B3636).withOpacity(0.75);
+const Color Purple = Color(0xFF9E00FF);
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+
+//category tags colors
+const Color categoryOneColor = Color(0xFF8E8FFA);
+const Color categoryTwoColor = Color(0xFFC2D9FF);  
+const Color categoryThreeColor = Color(0xFFFFE500);
+
+const Color MainNathuralWondersColor = Color(0xFF2EB200);
+final Color subNathuralWondersColor = Color(0xFF2EB200).withOpacity(0.53);
+
+const Color MainNightLifeColor = Color(0xFFFF9900);
+final Color subNightLifeColor = Color(0xFFFF9900).withOpacity(0.52);
+
+const Color MainLandmarkColor = Color(0xFF0029FF);
+final Color subLandmarkColor = Color(0xFF0029FF).withOpacity(0.66);
+final Color landMarkCardsBackgroundColor =Color(0xffCACACA).withOpacity(0.31);
+
+
+const Color mainCulturalColor = Color(0xFFC00C00);
+
+
+const Color mainGreenColor = Color(0xFF06FFA5);
+const Color mainRedlColor = Color(0xFFFF1E0F);
